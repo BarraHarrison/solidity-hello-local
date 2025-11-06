e0618df3c0fbaea8e8c6cc7d02db27c9da8a50a5
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-11-06 10:52:42 +0900

    The last deployed contract address may change if we re-deploy the solidity contract on ganache

--- scripts/interact.js
+++ scripts/interact.js
@@ -10,3 +10,17 @@ const signer = new ethers.Wallet(process.env.GANACHE_PRIVATE_KEY, provider);
 const contractPath = path.resolve(__dirname, "../contracts", "HelloBlockchain.sol");
 const source = fs.readFileSync(contractPath, "utf8");
 
+const input = {
+    language: "Solidity",
+    sources: {
+        "HelloBlockchain.sol": {
+            content: source
+        },
+    },
+    settings: { outputSelection: { "*": { "*": ["abi"] } } },
+};
+
+const compiled = JSON.parse(solc.compile(JSON.stringify(input)));
+const abi = compiled.contracts["HelloBlockchain.sol"]["HelloBlockchain"].abi;
+
+const CONTRACT_ADDRESS = "0xa0F88d1CCEEe8651631B01825B01CBE627150F16"
