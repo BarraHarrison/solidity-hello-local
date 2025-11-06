f5ec657d0d5c52d0e34fb7015067c6f8ac130dc5
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-11-06 10:48:16 +0900

    Connecting to the same local blockchain and compiling the solidity contract again

--- scripts/interact.js
+++ scripts/interact.js
@@ -4,3 +4,9 @@ const fs = require("fs");
 const path = require("path");
 const solc = require("solc");
 
+const provider = new ethers.JsonRpcProvider("http://127.0.0.1:8545");
+const signer = new ethers.Wallet(process.env.GANACHE_PRIVATE_KEY, provider);
+
+const contractPath = path.resolve(__dirname, "../contracts", "HelloBlockchain.sol");
+const source = fs.readFileSync(contractPath, "utf8");
+
